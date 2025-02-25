c074aa43ecc7b7ae70ece731bbab195cdf7caf7a
OtavioPascoal <fjoooosa@gmail.com>
2025-02-25 10:12:04 -0300

    Feat: Initial commit

---
+++ IventoryControl/src/main/java/com/Tavin/IventoryControl/IventoryControlApplication.java
@@ -0,0 +1,13 @@
+package com.Tavin.IventoryControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IventoryControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IventoryControlApplication.class, args);
+	}
+
+}
---
+++ IventoryControl/src/test/java/com/Tavin/IventoryControl/IventoryControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tavin.IventoryControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IventoryControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
